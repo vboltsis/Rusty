b2932abfaff818910bb1677959bfef690ad8d3d4
vboltsis <evangelos@wearedope.com>
2021-12-19 16:13:30 +0200

    Initial Commit

---
+++ Hello.rs
@@ -0,0 +1,15 @@
+fn main(){
+    println!("Hello, world!");
+    let number = fibonacci(20);
+    print!("{}", number);
+}
+//fibonacci
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
